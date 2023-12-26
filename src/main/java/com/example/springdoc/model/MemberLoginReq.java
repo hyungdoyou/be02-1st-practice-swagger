7a405d3a6f4b279e64fe45203f15ca1cee805bb9
hyungdoyou <yhd4286@gmail.com>
2023-12-27 00:01:30 +0900

    [Docs] 코드 수정
    코드 수정

--- src/main/java/com/example/springdoc/model/MemberLoginReq.java
+++ src/main/java/com/example/springdoc/model/MemberLoginReq.java
@@ -18,6 +18,7 @@ public class MemberLoginReq {
     private String email;
 
     @NotNull
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$")
     @Size(min= 1, max = 20) // 문자열 길이 지정
     @Max(10) // 숫자 크기 지정
     @ApiParam(value = "회원의 패스워드를 입력", required = true, example = "qwer1234")
